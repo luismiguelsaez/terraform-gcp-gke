26ae5fe406ee331a3a92c32d5e51cbb6f36ef422
luismiguelsaez <luismiguelsaez83@gmail.com>
2021-01-19 13:06:19 +0100

    Added version and jobs namespace name

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -3,9 +3,10 @@ resource "helm_release" "spark_operator" {
 
   repository = "https://googlecloudplatform.github.io/spark-on-k8s-operator"
   chart      = "spark-operator/spark-operator"
+  version    = "v1beta2-1.2.0-3.0.0"
 
   set {
     name  = "sparkJobNamespace"
-    value = "SparkCluster"
+    value = "sparkJobs"
   }
 }
