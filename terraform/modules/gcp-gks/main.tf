49370e1867c032bcd9aa6663da57c230c38e9c90
luismiguelsaez <luismiguelsaez83@gmail.com>
2021-02-08 20:07:22 +0100

    Modified cluster options

--- terraform/modules/gcp-gks/main.tf
+++ terraform/modules/gcp-gks/main.tf
@@ -1,7 +1,7 @@
-resource "google_service_account" "this" {
-  account_id   = format("gke-cluster-%s", var.cluster_name)
-  display_name = format("%s GKE Cluster service account", var.cluster_name)
-}
+#resource "google_service_account" "this" {
+#  account_id   = format("gke-cluster-%s", var.cluster_name)
+#  display_name = format("%s GKE Cluster service account", var.cluster_name)
+#}
 
 data "google_compute_zones" "available" {
   region  = var.region
@@ -11,12 +11,33 @@ locals {
   autoscaling_resource_limits = var.autoscaling_resource_enabled ? var.autoscaling_resource_limits : []
 }
 
+resource "random_string" "random" {
+  length  = 12
+  number  = false
+  lower   = true
+  upper   = false
+  special = false
+}
+
 resource "google_container_cluster" "this" {
-  name     = var.cluster_name
+  name     = format("%s-%s", var.cluster_name, random_string.random.result)
   location = var.region
 
   remove_default_node_pool = true
   initial_node_count       = var.control_node_number
+
+  node_config {
+    preemptible = true
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_write",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
 
   workload_identity_config {
     identity_namespace = "${var.project_id}.svc.id.goog"
@@ -36,7 +57,7 @@ resource "google_container_cluster" "this" {
 }
 
 resource "google_container_node_pool" "this" {
-  name       = var.cluster_name
+  name       = format("%s-%s", var.cluster_name, random_string.random.result)
   location   = var.region
   cluster    = google_container_cluster.this.name
   node_count = var.worker_node_number
@@ -46,13 +67,22 @@ resource "google_container_node_pool" "this" {
     max_node_count = 5
   }
 
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
   node_config {
     preemptible  = true
     machine_type = var.worker_node_instance_type
 
-    service_account = google_service_account.this.email
+    #service_account = google_service_account.this.email
     oauth_scopes    = [
-      "https://www.googleapis.com/auth/cloud-platform"
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_write",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring"
     ]
   }
 }
